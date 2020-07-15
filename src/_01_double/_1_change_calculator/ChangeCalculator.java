188739df6ddef5cc389759fc480d0de075590e8c
root <root@c0120df2edf6>
2020-07-15 21:57:24 +0000

    Change and Test scores

--- src/_01_double/_1_change_calculator/ChangeCalculator.java
+++ src/_01_double/_1_change_calculator/ChangeCalculator.java
@@ -12,17 +12,19 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
+String teller= JOptionPane.showInputDialog("How many Nickels do you have?");
 		// Convert their answer to an int.   Hint: Integer.parseInt()  
-
+int nickels=Integer.parseInt(teller);
 		// Ask the user how many dimes they have, and convert their answer
-
+String soquet= JOptionPane.showInputDialog("How many Dimes do you have?"); 
+int dimes=Integer.parseInt(soquet);
 		// Ask the user how many quarters they have, and convert their answer
-
+String vekoma = JOptionPane.showInputDialog("How many Quarters do you have?"); 
+int quarters=Integer.parseInt(vekoma);
 		// Calculate how much money the user has.  Hint: Use a double variable 
-
+double mack=nickels*.05+dimes*.10+quarters*.25;
 		// Tell the user how much money they have in dollars and cents format (e.g. $6.75)
-
+JOptionPane.showMessageDialog(null,"You have "+mack);
 	}
 }
 
---
+++ src/_01_double/_2_test_scores/TestScores.java
@@ -0,0 +1,32 @@
+package _01_double._2_test_scores;
+
+public class TestScores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
