37a790f3e11f879bf39e10d660939648a88c3451
root <root@7fca3caa4697>
2020-07-22 21:57:26 +0000

    Test Score

--- src/_01_double/_2_test_scores/TestScores.java
+++ src/_01_double/_2_test_scores/TestScores.java
@@ -1,10 +1,21 @@
 package _01_double._2_test_scores;
 
+import javax.swing.JOptionPane;
+
 public class TestScores {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+String testScore =JOptionPane.showInputDialog(null,"What did you get on the test?");
+		
+double score=Double.parseDouble(testScore);		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 	
